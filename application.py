e8c7843ecd0aec7580051bb72396c10ab153735c
kishanchand9989 <kishanchand9989@gmail.com>
2023-07-22 16:28:08 +0530

    app updated

--- application.py
+++ application.py
@@ -10,16 +10,15 @@ from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application =Flask(__name__)
 
-app =application
 
 ## Route for a home page
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/predictdata',methods=['GET','POST'])
+@application.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -48,5 +47,17 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0",debug=True)
 
+
+
+
+
+
+
+
+
+
+
+
+    
